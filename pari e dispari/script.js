6e6bd42f338b5430218aadebc657b81a9ffc5d65
MegaEmme <mikmusco@gmail.com>
2025-02-28 14:54:01 +0100

    add: pseudo-code

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,1 +1,19 @@
 console.log("Mike's Pari e Dispari")
+
+/*
+- chiedo all'utente di scegliere se pari o dispari
+- salvo la scelta in una variabile 
+- chiedo all'utente di inserire un numero da 1 a 5
+- calcolo un numero randomico da 1 a 5 con una **funzione**
+- sommo i due valori e li salvo in una variabile
+- creo una **funzione** isNumberOdd dal possibile valore *true* o *false*
+- **se** ho scelto dispari e la **funzione** isNumberOdd restituisce *true*
+    - stampo "hai vinto"
+- **altrimenti se** ho scelto dispari e la **funzione** isNumberOdd restituisce *false*
+    - stampo "hai perso"
+- **altrimenti se** ho scelto pari e la **funzione** isNumberOdd restituisce *true*
+    - stampo "hai perso"
+- **altrimenti**
+    - stampo "hai vinto"
+*/
+
