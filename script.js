cc15e6c34b0c8d6ce98338b6ec95014f5e54e198
MegaEmme <mikmusco@gmail.com>
2025-02-28 14:04:02 +0100

    add: pseudo-code

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Mike');
+console.log("Mike's Palindrome");
+
+/*
+- chiedo all'utente di inserire una parola
+- la salvo in una variabile
+- creo la funzione
+    - divido la parola inserita in singoli caratteri (.split) di un array
+    - inverto i valori dell'array (.reverse)
+    - riunisco i valori dell'array in una stringa (.join)
+    - **se** la nuova parola è uguale a quella inserita dall'utente
+        - stampo "la parola è palindroma"
+    - **altrimenti**
+        - stampo "la parola non è palindroma"
+*/
+
+const userWord = prompt('Inserisci una parola');
+
+
